3b0607456ca3588aba123ab7dbaaadfd69f7828a
Bohdan Yatsuk <yatsukbogdan@gmail.com>
2026-09-02 11:47:56 +0100

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    dfs(root, arr = [], level = 0) {
+        if (root == null) return
+        this.dfs(root.left, arr, level + 1)
+        if (arr[level] == null) {
+            arr[level] = []
+        }
+        arr[level].push(root.val)
+        this.dfs(root.right, arr, level + 1)
+    }
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root) {
+        const arrs = []
+        this.dfs(root, arrs, 0)
+        return arrs.map(arr => arr.at(-1))
+    }
+}
